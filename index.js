bf59dbfd110e94d8473f0981e7a7243411c15d94
ahasanshaker <ahasanullalshaker@gmail.com>
2025-09-07 01:39:14 +0600

    get job data from server

--- index.js
+++ index.js
@@ -27,6 +27,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const courseCollection=client.db("courseManage").collection('courses')
+
+    // Courses api
+    app.get('/courses', async(req,res)=>{
+        const cursor = courseCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+     
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
